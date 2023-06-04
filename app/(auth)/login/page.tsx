a5b427f647a8b217a56aaab51c6c20bdf0d49176
Vishal <74290663+dr-cyberx@users.noreply.github.com>
2023-06-04 15:38:23 +0530

    handle loading in server components

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,9 +2,11 @@
 import React from 'react'
 import axios from 'axios';
 
+const wait = (ms:number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 const Login = async () => {
+  await wait(5000)
   const {data} = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
-  console.log(data)
   return (
     <div> {JSON.stringify(data)}</div>
   )
